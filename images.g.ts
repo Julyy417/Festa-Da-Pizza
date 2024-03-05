ad7ea17931b9c4c8b911a992a5044eb99ad76fb7
Julyy417 <162327213+Julyy417@users.noreply.github.com>
2024-03-05 10:22:04 -0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "EM~exxT809P9NBm_MJip":
+            case "musiquinha":return hex`003c000408010205001c000f0a006400f4010a000004000000000000000000000000000000000212000000040001200c001000012014001800012007001c00020a006400f40164000004000000000000000000000000000000000306001c0020000120`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
